53f44e232f95f11dde029680e5287e989369d3e7
wildmanwang <cliff.w@qq.com>
2019-12-25 18:30:50 +0800

    my first django

---
+++ dJangoBase/app_test/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def app_test_hello(request):
+    return HttpResponse("<h1>hello app test!")
